2118c9bf7a4c1860e04ccbd7f369ad9a4103d693
Satit Rianpit <siteslave@msn.com>
2022-09-28 11:43:06 +0700

    add camera, permission

--- lib/e-kyc.dart
+++ lib/e-kyc.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:my_moph/register.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class EKycPage extends StatefulWidget {
   const EKycPage({Key? key}) : super(key: key);
@@ -9,6 +11,43 @@ class EKycPage extends StatefulWidget {
 }
 
 class _EKycPageState extends State<EKycPage> {
+  bool isGrantCamera = false;
+
+  Future takePicture() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? pickedFile = await _picker.pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+    );
+
+    print(pickedFile!.path);
+  }
+
+  Future getPermissionStatus() async {
+    await Permission.camera.request();
+    var status = await Permission.camera.status;
+    if (status.isGranted) {
+      print('Camera Permission: GRANTED');
+      setState(() {
+        isGrantCamera = true;
+      });
+      // Set and initialize the new camera
+    } else {
+      setState(() {
+        isGrantCamera = false;
+      });
+      print('Camera Permission: DENIED');
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    getPermissionStatus();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +56,17 @@ class _EKycPageState extends State<EKycPage> {
       ),
       body: ListView(
         children: [
+          isGrantCamera
+              ? Container()
+              : Container(
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.all(20),
+                  decoration: BoxDecoration(color: Colors.yellow),
+                  child: Text(
+                    'ไม่สามารถเข้าถึงกล้องได้',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.red),
+                  )),
           Container(
             margin: EdgeInsets.all(20),
             child: Column(
@@ -24,16 +74,23 @@ class _EKycPageState extends State<EKycPage> {
               children: [
                 Text('Please upload Face'),
                 Center(
-                  child: Container(
-                    height: 90,
-                    width: 90,
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black12, width: 2),
-                        color: Colors.grey[50],
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage("assets/images/customer.png"))),
+                  child: GestureDetector(
+                    onTap: () {
+                      if (isGrantCamera) {
+                        takePicture();
+                      }
+                    },
+                    child: Container(
+                      height: 90,
+                      width: 90,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black12, width: 2),
+                          color: Colors.grey[50],
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage("assets/images/customer.png"))),
+                    ),
                   ),
                 ),
               ],
